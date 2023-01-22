ee309a09f900aef74f41a35803a48d6f0d88bcd0
mrasamny <mrasamny@desu.edu>
2023-01-21 22:27:19 -0500

    corrected ports

--- broadcast.py
+++ broadcast.py
@@ -5,8 +5,9 @@ import pickle
 if __name__ == "__main__":
 
     hostname = socket.gethostname()
-    port = 12000
+    broadcast_port = 13000
     ip = comm.get_ip()
+    server_port = 12000
 
     if len(ip) == 0:
         print('No interface found that has an IP address')
@@ -16,7 +17,7 @@ if __name__ == "__main__":
         # start broadcast server and wait for query
         broadcast_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         broadcast_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        broadcast_server.bind(('0.0.0.0', port))
+        broadcast_server.bind(('0.0.0.0', broadcast_port))
     except Exception as e:
         print(e)
         exit(2)
@@ -25,7 +26,7 @@ if __name__ == "__main__":
         try:
             (message, address) = broadcast_server.recvfrom(1024)
             if message.decode() == hostname:
-                response = pickle.dumps((ip, port))
+                response = pickle.dumps((ip, server_port))
                 broadcast_server.sendto(response, address)
         except Exception as e:
             print(e)
