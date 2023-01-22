ac432cef946c49add46cc453b23742539f5b0d2f
mrasamny <mrasamny@desu.edu>
2023-01-22 14:13:36 -0500

    made use of get_broadcast_addr method

--- client.py
+++ client.py
@@ -4,10 +4,11 @@ import comm
 if __name__ == '__main__':
     # ip = input('Enter IP address:')
     # port = int(input('Enter port: '))
-    # ip = comm.get_ip()
+    ip = comm.get_ip()
     # port = 12000
-    message = 'Marwans-MacBook-Pro.local'
-    server_addr = comm.send_discover_message(('192.168.7.255', 13000), message)
+    broadcast_addr = comm.get_broadcast_addr(ip)
+    message = 'pacmaners'
+    server_addr = comm.send_discover_message((broadcast_addr, 13000), message)
     print(f'BROADCAST RESPONSE: {server_addr}')
 
     message = 'goodbye'
